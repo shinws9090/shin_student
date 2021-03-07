fe3238190e6c608568cfdf474f578b2f01614bee
newspringjeong <newspringjeong@DESKTOP-5K5T4JB>
2021-03-07 20:47:31 +0900

    ?

--- src/main/java/shin/student2/StdLHLeftPanel.java
+++ src/main/java/shin/student2/StdLHLeftPanel.java
@@ -1,14 +1,41 @@
 package shin.student2;
 
 import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class StdLHLeftPanel extends JPanel {
+	private JTextField textField;
 
 	/**
 	 * Create the panel.
 	 */
 	public StdLHLeftPanel() {
 		setBounds(100, 100, 263, 690);
+		setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		add(panel, BorderLayout.NORTH);
+		panel.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		panel.add(lblNewLabel);
+		
+		textField = new JTextField();
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("New button");
+		panel.add(btnNewButton);
+		
+		JPanel panel_1 = new JPanel();
+		add(panel_1, BorderLayout.CENTER);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		panel_1.add(btnNewButton_1);
 	}
 
 }
